770c8ee8845d11e1563bd0ae37037a40ef322338
atul kumar <atul@zoabe.com>
2021-02-08 17:10:40 +0530

    updated

--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
 import './index.css';
 
-const Modal = () => {
+const Modal = ({closeModal}) => {
 
   useEffect(() => {
     window.addEventListener('beforeunload', function (e) {
+      closeModal();
       // Cancel the event
       e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
       // Chrome requires returnValue to be set
       e.returnValue = '';
     });
-  }, [])
+  }, [closeModal])
 
   return (
     <div className>
